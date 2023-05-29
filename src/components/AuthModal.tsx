932c4d376233a24b466f5980a3ae96d40459dd2c
J Hurt <hurtsec@gmail.com>
2023-05-28 21:58:26 -0400

    refactor auth modal to use passwordless options

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,6 +1,4 @@
 import { XCircleIcon } from "@heroicons/react/24/outline";
-import SignUpForm from "./SignUpForm";
-import SignInForm from "./SignInForm";
 
 const AuthModal = (props: {
   isSignUp: boolean;
@@ -9,25 +7,32 @@ const AuthModal = (props: {
   const { isSignUp, handleAuthModalClick } = props;
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-hidden overscroll-contain bg-neutral-900 bg-opacity-30">
-      <div className="relative flex w-full max-w-lg flex-col items-center rounded-xl bg-white p-10">
+      <div className="relative flex w-full max-w-lg flex-col items-center rounded-xl bg-neutral-900 p-10">
         <div
-          className="absolute right-3 top-3 w-5 cursor-pointer"
+          className="absolute right-3 top-3 w-5 cursor-pointer text-neutral-500"
           onClick={handleAuthModalClick}
         >
           <XCircleIcon />
         </div>
-        <h2 className="text-2xl font-bold uppercase italic">
-          {isSignUp ? "Create Account" : "Log In"}
+        <h2 className="text-2xl font-bold uppercase italic text-neutral-100">
+          {isSignUp ? "Create Account" : "Get Started"}
         </h2>
-        <p className="py-3 text-center font-semibold">
+        <p className="py-3 text-center font-semibold text-neutral-100">
           By clicking Log In, you agree to our terms. Learn how we process your
           data in our Privacy Policy and Cookie Policy.
         </p>
-        {isSignUp ? <SignUpForm /> : <SignInForm />}
-        <div className="w-full pb-4 pt-8">
+        <button className="mb-4 w-full rounded-full border-2 border-neutral-500 py-1 text-2xl font-bold text-neutral-500 hover:bg-neutral-800">
+          Log in with Facebook
+        </button>
+        <button className="mb-4 w-full rounded-full border-2 border-neutral-500 py-1 text-2xl font-bold text-neutral-500 hover:bg-neutral-800">
+          Log in with Email
+        </button>
+        <div className="w-full pb-4 pt-4">
           <hr className="border-t-2 border-neutral-500" />
         </div>
-        <h2 className="text-2xl font-bold uppercase italic">Get The App</h2>
+        <h2 className="text-2xl font-bold uppercase italic text-neutral-100">
+          Get The App
+        </h2>
       </div>
     </div>
   );
