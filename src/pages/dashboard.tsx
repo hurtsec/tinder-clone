95d26e4b23b5bfa458048f90c258c25542da5bdb
J Hurt <hurtsec@gmail.com>
2023-05-30 23:28:03 -0400

    hook retrieved user data up to card

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -10,24 +10,29 @@ const Dashboard = ({ currentUser }: { currentUser: User }) => {
     required: true,
     onUnauthenticated: () => void router.replace("/"),
   });
-  const { data: usersGenderInterestOverlap, isLoading: isLoadingUsers } =
+  const { data: whoAmI } = api.users.whoAmI.useQuery();
+  const { data: usersGenderInterestOverlap } =
     api.users.getByGenderInterests.useQuery();
-
-  console.log("usersGenderInterestOverlap :>> ", usersGenderInterestOverlap);
   return (
     <div className="flex gap-5">
-      <ChatContainer user={currentUser} />
+      <ChatContainer user={whoAmI} />
       <div className="flex w-8/12 flex-grow flex-col items-center p-12">
         <div
           className="flex flex-col justify-end overflow-hidden rounded-2xl bg-white bg-cover bg-center shadow-2xl shadow-black"
           style={{
-            backgroundImage: `url(https://i.imgur.com/Q9WPlWA.jpeg)`,
+            backgroundImage: usersGenderInterestOverlap
+              ? `url(${usersGenderInterestOverlap[0]?.image || ""})`
+              : "",
             width: "400px",
             height: "650px",
           }}
         >
           <div className="bg-white p-4">
-            <h3 className="font-semibold">Richard Heathclif</h3>
+            <h3 className="font-semibold">
+              {usersGenderInterestOverlap
+                ? usersGenderInterestOverlap[0]?.name
+                : ""}
+            </h3>
           </div>
         </div>
       </div>
